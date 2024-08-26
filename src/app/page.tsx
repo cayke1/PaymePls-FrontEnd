7a608f87623b142f9f966d7ab2dc49b3606f8dd3
Cayke Veras <caykedanielcd@gmail.com>
2024-08-26 09:29:46 -0300

    feat: homepage redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+"use client"
+import { useEffect } from "react";
+import { useRedirect } from "./hooks/useRedirect"
+
 export default function Component() {
+  const redirect = useRedirect();
+  useEffect(() => {
+    redirect.to("/login");
+  })
   return (
     <h2>a</h2>
   )
